4cad4efca177b6b150ab26448c2ec3fe1134fc30
John Merz <jmerz826@gmail.com>
2022-02-05 18:17:27 -0500

    can see db in pg!

---
+++ back-end/migrations/20220205225851_firstMigration.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.createTable("game1", (table) => {
+    table.increments()
+    table.string("user", 128).notNullable()
+    table.integer("score").notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("game1")
+}
