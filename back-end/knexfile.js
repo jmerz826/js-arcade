e904563efd6d085e44669061be544f813de482df
John Merz <jmerz826@gmail.com>
2022-02-05 17:46:28 -0500

    configuring postgres

---
+++ back-end/knexfile.js
@@ -0,0 +1,38 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = {rejectUnauthorized: false}
+}
+
+const sharedConfig = {
+  client: 'pg',
+  migrations: { direcory: './api/data/migrations' },
+  seeds: {directory: './api/seeds'}
+}
+
+module.exports = {
+
+  development: {
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL
+  },
+  testing: {
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL
+  },
+
+  production: {
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+
+}
