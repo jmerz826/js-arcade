6a560034ef4b2a7d045cb46581139c131adbab1a
John Merz <jmerz826@gmail.com>
2022-02-06 14:05:26 -0500

    made home page file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
+import HomePage from "./components/homePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route exact path='/' element={ <HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ front-end/src/components/homePage.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+const StyledHomePage = styled.div`
+
+`
+
+const HomePage = (props) => {
+    return (
+        <StyledHomePage>
+            <h1>hi</h1>
+        </StyledHomePage>        
+    )
+}
+
+export default HomePage;
