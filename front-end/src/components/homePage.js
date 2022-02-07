df6a6cfb3488b0bb9d0afb24c63121aa208e2347
John Merz <jmerz826@gmail.com>
2022-02-06 20:42:25 -0500

    homePage content done

--- front-end/src/components/homePage.js
+++ front-end/src/components/homePage.js
@@ -18,12 +18,33 @@ const HomePage = (props) => {
       <p>
         You have entered John's JavaScript arcade! Have fun playing around with
         the games I have created utilizing the knowledge I've acquired from my
-        Full Stack bootcamp. <br />
+        Full Stack bootcamp. Games can be found in the navigation bar at the top
+        of the page. <br />
         All games have a leader board that is running on a backend server that I
         created for this project.
       </p>
       <h3>Creator</h3>
       <p>This page was created solely by John Merz</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/john-merz/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            John's Linkedin
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/jmerz826"
+            target="_blank"
+            rel="noreferrer"
+          >
+            John's GitHub
+          </a>
+        </li>
+      </ul>
     </StyledHomePage>
   );
 };
