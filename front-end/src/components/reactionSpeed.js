e248e61a9455d8de5678a1a078b11760dca727ea
John Merz <jmerz826@gmail.com>
2022-02-06 22:43:58 -0500

    progress on reaction speed game

--- front-end/src/components/reactionSpeed.js
+++ front-end/src/components/reactionSpeed.js
@@ -1,23 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import ReactionSpeedGame from "../games/reactionSpeed";
 
 const StyledDiv = styled.div``;
 
 const ReactionSpeed = (props) => {
-    const [gameStarted, setGameStarted] = useState(false)
+    const [gameStarted, setGameStarted] = useState(false);
+
+  const handleStart = () => {
+    setGameStarted(true);
+  };
   return (
     <StyledDiv>
       <h2>Reaction Speed Game</h2>
       <h3>How to play</h3>
       <p>
         As it sounds, the Reaction Speed Game will test your{" "}
-              <span style={{ fontStyle: "italic" }}>reaction speed</span>. <br />
-              Upon starting the game, each round will consist of a button appearing shortly; you must click this button as quickly as possible. The game consists of 5 rounds 
+        <span style={{ fontStyle: "italic" }}>reaction speed</span>. <br />
+        Upon starting the game, each round will consist of a button appearing
+        shortly; you must click this button as quickly as possible. The game
+        consists of 5 rounds. Good luck!
       </p>
       <div className="leaderboard">
         <h3>Leaderboard</h3>
       </div>
-      <div id="reaction-speed-game"></div>
+      <div id="reaction-speed-game">
+        <button id="start-game-btn" onClick={handleStart}>
+          Start Game
+        </button>
+              {gameStarted && <ReactionSpeedGame setGameStarted={setGameStarted}/>}
+      </div>
     </StyledDiv>
   );
 };
--- front-end/src/games/reactionSpeed.js
+++ front-end/src/games/reactionSpeed.js
@@ -1,5 +1,43 @@
-const reactionSpeedGame = () => {
-    
-}
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-export default reactionSpeedGame;
+const StyledDiv = styled.div``;
+
+const ReactionSpeedGame = (props) => {
+    const [shown, setShown] = useState(false);
+    const [delay, setDelay] = useState(pickRandomDelay());
+    const [roundsFinished, setRoundsFinished] = useState(0);
+
+    useEffect(() => {
+        setDelay(pickRandomDelay());
+    }, [])
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setShown(true)
+        }, delay)
+    })
+
+    function pickRandomDelay() {
+        return 1000 * (Math.random() * 4 + 1)
+    }
+
+    function handleClick() {
+        setRoundsFinished(roundsFinished + 1)
+        setShown(false)
+        if (roundsFinished === 4) {
+            props.setGameStarted(false)
+        }
+        setDelay(pickRandomDelay())
+    }
+
+
+  return (
+    <StyledDiv>
+      <h4>The game has begun!</h4>
+      {shown && <button onClick={handleClick}>Click here!</button>}
+    </StyledDiv>
+  );
+};
+
+export default ReactionSpeedGame;
