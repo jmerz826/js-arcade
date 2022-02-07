a5f12982612272fd8f07ae73e38157001190d627
John Merz <jmerz826@gmail.com>
2022-02-06 21:28:36 -0500

    beginning reaction speed game

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
 import HomePage from "./components/homePage";
+import ReactionSpeed from "./components/reactionSpeed";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route exact path='/' element={ <HomePage />} />
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/reaction-speed' element={<ReactionSpeed/>} />
       </Routes>
     </div>
   );
---
+++ front-end/src/games/reactionSpeed.js
@@ -0,0 +1,5 @@
+const reactionSpeedGame = () => {
+    
+}
+
+export default reactionSpeedGame;
