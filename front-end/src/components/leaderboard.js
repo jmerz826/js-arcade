df41a80cf624191050237ee730acd7f2ab335d4d
John Merz <jmerz826@gmail.com>
2022-02-08 10:56:36 -0500

    made leaderboard component

---
+++ front-end/src/components/leaderboard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components"; 
+
+const StyledDiv = styled.div``
+
+const Leaderboard = (props) => {
+    const { highScores } = props;
+    return (
+        <StyledDiv>
+            <ul>
+                {highScores.map(entry => {
+                    return (
+                        <li>Player: {entry.name}: {entry.score} </li>
+                    )
+                })}
+            </ul>
+        </StyledDiv>
+    );
+};
+
+export default Leaderboard;
--- front-end/src/components/reactionSpeed.js
+++ front-end/src/components/reactionSpeed.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ReactionSpeedGame from "../games/reactionSpeed";
+import Leaderboard from "./leaderboard";
 
 const StyledDiv = styled.div``;
+const dummyScores = [
+  { name: 'Jerry', score: 123456 },
+  {name: 'George', score: 654321},
+  {name: 'Elaine', score: 987654},
+  {name: 'Kramer', score: 9876543},
+  { name: 'Larry', score: 9999999 },
+  {name: 'Newman', score: 99999999999995}
+]
 
 const ReactionSpeed = (props) => {
   const [gameStarted, setGameStarted] = useState(false);
   const [totalTime, setTotalTime] = useState(0);
+  const [highScores, setHighScores] = useState([]);
+
+  // populate 
+  useEffect(() => {
+    // replace with API call
+    const top5 = dummyScores.sort((a, b) => a.score - b.score).slice(0,5)
+    setHighScores(top5);
+  }, [])
 
   const handleStart = () => {
     if (totalTime !== 0) {
@@ -14,6 +31,10 @@ const ReactionSpeed = (props) => {
     }
     setGameStarted(true);
   };
+
+  const processScore = () => {
+    console.log(highScores);
+  }
   return (
     <StyledDiv>
       <h2>Reaction Speed Game</h2>
@@ -27,6 +48,7 @@ const ReactionSpeed = (props) => {
       </p>
       <div className="leaderboard">
         <h3>Leaderboard</h3>
+        <Leaderboard highScores={highScores}/>
       </div>
       <div id="reaction-speed-game">
         {!gameStarted && <button id="start-game-btn" onClick={handleStart}>
@@ -38,6 +60,7 @@ const ReactionSpeed = (props) => {
             setGameStarted={setGameStarted}
             totalTime={totalTime}
             setTotalTime={setTotalTime}
+            processScore={processScore}
           />
         )}
       </div>
