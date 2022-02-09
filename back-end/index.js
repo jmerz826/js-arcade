fce0a0ec7d112e76eb1f56a95196402fc5463413
John Merz <jmerz826@gmail.com>
2022-02-09 10:54:37 -0500

    trigger rebuild

--- back-end/index.js
+++ back-end/index.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 9000
 const server = require("./api/server")
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
