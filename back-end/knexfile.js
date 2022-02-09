4096b01a5cf4a99d4771bbfb55ae341e2b11f75a
John Merz <jmerz826@gmail.com>
2022-02-09 11:04:11 -0500

    changed prod url

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
     ...sharedConfig,
-    connection: process.env.DEV_DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
