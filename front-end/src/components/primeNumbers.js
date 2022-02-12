7f9f2fdc7520558f1cc5bae5c3ba8e3d3561aaf8
John Merz <jmerz826@gmail.com>
2022-02-11 23:48:19 -0500

    prime numbers game moved to own file

--- front-end/src/components/primeNumbers.js
+++ front-end/src/components/primeNumbers.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Leaderboard from "./leaderboard";
 import axios from "axios";
 import { newScoreAxios } from "../auth/axiosWithAuth";
+import PrimeNumbersGame from "../games/primeNumbers";
 
 const StyledDiv = styled.div``;
 
@@ -17,192 +18,170 @@ const dummyScores = [
 
 const PrimeNumbers = (props) => {
   const [highScores, setHighScores] = useState([]);
-    const [gameStarted, setGameStarted] = useState(false);
-    const [lowerBound, setLowerBound] = useState(0)
-    const [upperBound, setUpperBound] = useState(0)
-    const [roundsCompleted, setRoundsCompleted] = useState(0)
-    const [correctAnswer, setCorrectAnswer] = useState(0)
-    const [userGuess, setUserGuess] = useState('')
-    const [lastGameScore, setLastGameScore] = useState(0)
-    const [showCorrectMessage, setShowCorrectMessage] = useState(false)
-    const [countdown, setCountdown] = useState(0)
-    const [newHighScoreFlag, setNewHighScoreFlag] = useState(false)
-    const [userName, setUserName] = useState('')
-    
+  const [gameStarted, setGameStarted] = useState(false);
+  const [lowerBound, setLowerBound] = useState(0);
+  const [upperBound, setUpperBound] = useState(0);
+  const [roundsCompleted, setRoundsCompleted] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+  const [userGuess, setUserGuess] = useState("");
+  const [lastGameScore, setLastGameScore] = useState(0);
+  const [showCorrectMessage, setShowCorrectMessage] = useState(false);
+  const [countdown, setCountdown] = useState(0);
+  const [newHighScoreFlag, setNewHighScoreFlag] = useState(false);
+  const [userName, setUserName] = useState("");
 
-    useEffect(() => {
-        axios.get('https://js-arcade.herokuapp.com/api/prime-numbers')
-            .then(res => {
-                const top5 = res.data.slice(0, 5)
-                setHighScores(top5)
-                setBounds()
-            })
-            .catch(err => console.error(err))
-    }, [newHighScoreFlag]) //eslint-disable-line
+  useEffect(() => {
+    axios
+      .get("https://js-arcade.herokuapp.com/api/prime-numbers")
+      .then((res) => {
+        const top5 = res.data.slice(0, 5);
+        setHighScores(top5);
+        setBounds();
+      })
+      .catch((err) => console.error(err));
+  }, [newHighScoreFlag]); //eslint-disable-line
 
-    useEffect(() => {
-        let res = 0;
-        for (let i = lowerBound; i <= upperBound; i++){
-            isPrime(i) && res++
-        }
-        setCorrectAnswer(res)
-    }) //eslint-disable-line
+  useEffect(() => {
+    let res = 0;
+    for (let i = lowerBound; i <= upperBound; i++) {
+      isPrime(i) && res++;
+    }
+    setCorrectAnswer(res);
+  }); //eslint-disable-line
 
-    useEffect(() => {
-        if (showCorrectMessage) {
-            setTimeout(() => {
-                setShowCorrectMessage(false)
-            }, 750) 
-        }
-    }, [showCorrectMessage])
+  useEffect(() => {
+    if (showCorrectMessage) {
+      setTimeout(() => {
+        setShowCorrectMessage(false);
+      }, 750);
+    }
+  }, [showCorrectMessage]);
 
-    useEffect(() => {
-      if (lastGameScore > 0) {
-        const lowestHighScore = highScores.slice(-1)[0];
-        if (lastGameScore > lowestHighScore.score) {
-            setNewHighScoreFlag(true);
-            issueToken()
-        }
+  useEffect(() => {
+    if (lastGameScore > 0) {
+      const lowestHighScore = highScores.slice(-1)[0];
+      if (lastGameScore > lowestHighScore.score) {
+        setNewHighScoreFlag(true);
+        issueToken();
       }
-    }, [lastGameScore]);
-
-    useEffect(() => {
-        if (countdown > 0) {
-            const timer = setTimeout(() => {
-                setCountdown(countdown - 1)
-            }, 1000);
-            return () => clearTimeout(timer)
-        } else {
-            setLastGameScore(roundsCompleted)
-            setGameStarted(false)
-        }
-    }, [countdown])
-
-    // const tick = () => {
-    //     setTimeout(() => {
-    //         setCountdown(countdown - 1)
-    //     }, 1000)
-    // }
-
-    function setBounds() {
-        const randomNumber = Math.floor(Math.random() * 50 + 1)
-        if (randomNumber === lowerBound) {
-            return setBounds()
-        }
-        const toAdd = roundsCompleted + ((roundsCompleted > 4) ? 2 : 1)
-        setLowerBound(randomNumber)
-        setUpperBound(randomNumber + 4 + toAdd)
-        
     }
+  }, [lastGameScore]);
 
-    function isPrime(num) {
-        if (num === 1) return false
-        for (let i = 2; i < num; i++){
-            if (num % i === 0) return false
-        }
-        return true
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setTimeout(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setLastGameScore(roundsCompleted);
+      setGameStarted(false);
     }
+  }, [countdown]);
 
-    const handleChange = (e) => {
-        if (e.target.name === 'guess') {
-            setUserGuess(e.target.value)
-        }
-        if (e.target.name === 'name') {
-            setUserName(e.target.value)
-        }   
+  function setBounds() {
+    const randomNumber = Math.floor(Math.random() * 50 + 1);
+    if (randomNumber === lowerBound) {
+      return setBounds();
     }
+    const toAdd = roundsCompleted + (roundsCompleted > 4 ? 2 : 1);
+    setLowerBound(randomNumber);
+    setUpperBound(randomNumber + 4 + toAdd);
+  }
 
-    async function issueToken() {
-        await axios.post('https://js-arcade.herokuapp.com/api/auth/high-score', { score: lastGameScore })
-            .then(resp => {
-                localStorage.setItem('score-token', resp.data.token)
-            })
-            .catch(err => console.error(err))
+  function isPrime(num) {
+    if (num === 1) return false;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
     }
+    return true;
+  }
 
-    const handleSubmitGuess = (e) => {
-        e.preventDefault()
-        if (userGuess === String(correctAnswer)) {
-            setRoundsCompleted(roundsCompleted + 1)
-            setShowCorrectMessage(true)
-            setCountdown(10)
-        } else {
-            setLastGameScore(roundsCompleted)
-            setGameStarted(false)
-            setCountdown(0)
-        }
-        setBounds()
-        setUserGuess('')
+  const handleChange = (e) => {
+    if (e.target.name === "guess") {
+      setUserGuess(e.target.value);
     }
+    if (e.target.name === "name") {
+      setUserName(e.target.value);
+    }
+  };
 
-    const handleSubmitNewHighScore = async (e) => {
-        e.preventDefault()
-        // API post
-        await newScoreAxios().post('api/prime-numbers', { name: userName, score: lastGameScore })
-            .then(() => {
-                setUserName('')
-                setNewHighScoreFlag(false)
-            })
-            .catch(err => console.error(err))
+  async function issueToken() {
+    await axios
+      .post("https://js-arcade.herokuapp.com/api/auth/high-score", {
+        score: lastGameScore,
+      })
+      .then((resp) => {
+        localStorage.setItem("score-token", resp.data.token);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  const handleSubmitGuess = (e) => {
+    e.preventDefault();
+    if (userGuess === String(correctAnswer)) {
+      setRoundsCompleted(roundsCompleted + 1);
+      setShowCorrectMessage(true);
+      setCountdown(10);
+    } else {
+      setLastGameScore(roundsCompleted);
+      setGameStarted(false);
+      setCountdown(0);
     }
-    
-    const handleStart = () => {
-        setLastGameScore(0)
-        setRoundsCompleted(0)
-        setCountdown(10)
-        setGameStarted(true)
-    }
+    setBounds();
+    setUserGuess("");
+  };
+
+  const handleSubmitNewHighScore = async (e) => {
+    e.preventDefault();
+    // API post
+    await newScoreAxios()
+      .post("api/prime-numbers", { name: userName, score: lastGameScore })
+      .then(() => {
+        setUserName("");
+        setNewHighScoreFlag(false);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleStart = () => {
+    setLastGameScore(0);
+    setRoundsCompleted(0);
+    setCountdown(10);
+    setGameStarted(true);
+  };
   return (
     <StyledDiv>
       <h2>Prime Numbers Game</h2>
       <h3>How to play</h3>
       <p>
         The Prime Numbers Game will get progressively more challenging as the
-        game goes on.<br/> Each round, you will be given an inclusive range of
-        numbers. You must enter how many prime numbers fall within that range (Example. Range: 3-5, answer: 2).<br/> You have ten seconds to submit a guess each round.
-        Good luck!
+        game goes on.
+        <br /> Each round, you will be given an inclusive range of numbers. You
+        must enter how many prime numbers fall within that range (Example.
+        Range: 3-5, answer: 2).
+        <br /> You have ten seconds to submit a guess each round. Good luck!
       </p>
       <div className="leaderboard">
         <h3>Leaderboard</h3>
         <Leaderboard highScores={highScores} />
           </div>
-          <div id="prime-numbers-game">
-              {!gameStarted &&
-                  <div>
-                      <button onClick={handleStart}>Start Game</button>
-                      <h5>Last game's score: {lastGameScore}</h5>
-                  </div>
-              }
-              {gameStarted &&
-                  <div>
-                      <h3>The game has begun!</h3>
-                      <h4>{countdown}</h4>
-                      <h4>SCORE: {roundsCompleted}</h4>
-                      {<h4>range: {lowerBound} - {upperBound}</h4>}
-                      <form autoComplete="off">
-                          <input
-                              name='guess'
-                              value={userGuess}
-                              onChange={handleChange}
-                              autoComplete='off'
-                          />
-                          <button onClick={handleSubmitGuess}>Submit guess!</button>
-                      </form>
-                      {showCorrectMessage && <p>correct!</p>}
-                  </div>
-              }
-              {newHighScoreFlag && 
-                  <form>
-                      <h4>New High Score!</h4>
-                      <input
-                          name='name'
-                          value={userName}
-                          onChange={handleChange}
-                      />
-                      <button onClick={handleSubmitNewHighScore}>Submit</button>
-                </form>
-              }
-          </div>
+          <PrimeNumbersGame
+          gameStarted = {gameStarted}
+          handleStart = {handleStart}
+          lastGameScore = {lastGameScore}
+          countdown = {countdown}
+          roundsCompleted = {roundsCompleted}
+          lowerBound = {lowerBound}
+          upperBound = {upperBound}
+          userGuess = {userGuess}
+          handleChange = {handleChange}
+          handleSubmitGuess = {handleSubmitGuess}
+          showCorrectMessage = {showCorrectMessage}
+          newHighScoreFlag = {newHighScoreFlag}
+          userName = {userName}
+          handleSubmitNewHighScore = {handleSubmitNewHighScore}
+          />
     </StyledDiv>
   );
 };
--- front-end/src/games/primeNumbers.js
+++ front-end/src/games/primeNumbers.js
@@ -3,3 +3,62 @@ import styled from "styled-components";
 
 const StyledDiv = styled.div``
 
+const PrimeNumbersGame = (props) => {
+    const {
+        gameStarted,
+        handleStart,
+        lastGameScore,
+        countdown,
+        roundsCompleted,
+        lowerBound,
+        upperBound,
+        userGuess,
+        handleChange,
+        handleSubmitGuess,
+        showCorrectMessage,
+        newHighScoreFlag,
+        userName,
+        handleSubmitNewHighScore
+    } = props;
+    return (
+        <div id="prime-numbers-game">
+              {!gameStarted &&
+                  <div>
+                      <button onClick={handleStart}>Start Game</button>
+                      <h5>Last game's score: {lastGameScore}</h5>
+                  </div>
+              }
+              {gameStarted &&
+                  <div>
+                      <h3>The game has begun!</h3>
+                      <h4>{countdown}</h4>
+                      <h4>SCORE: {roundsCompleted}</h4>
+                      {<h4>range: {lowerBound} - {upperBound}</h4>}
+                      <form autoComplete="off">
+                          <input
+                              name='guess'
+                              value={userGuess}
+                              onChange={handleChange}
+                              autoComplete='off'
+                          />
+                          <button onClick={handleSubmitGuess}>Submit guess!</button>
+                      </form>
+                      {showCorrectMessage && <p>correct!</p>}
+                  </div>
+              }
+              {newHighScoreFlag && 
+                  <form>
+                      <h4>New High Score!</h4>
+                      <input
+                          name='name'
+                          value={userName}
+                          onChange={handleChange}
+                      />
+                      <button onClick={handleSubmitNewHighScore}>Submit</button>
+                </form>
+              }
+          </div>
+    )
+}
+
+export default PrimeNumbersGame
